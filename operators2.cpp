f940cdc6da84a9f74a42c9337ecf45f77473e196
Harish-kumar12345 <harishkumarup286@gmail.com>
2024-06-24 14:54:08 +0530

    Add files via upload

---
+++ operators2.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+   int a ,b;
+   cout<<"enter the value of a, b";
+   cin>>a>>b;
+   cout<<"Arthmetic operatop"<<endl;
+   cout<<"the value  of 'a+b' "<<a+b<<endl;
+    cout<<"the value  of 'a-b' "<<a-b <<endl;
+    cout<<"the value of a<=b " <<(a<=b)<<endl;
+    cout<<"the value of a<=b " <<(a<=b)<<endl;
+    cout<<"the value of a==b " <<(a==b)<<endl;
+    cout<<"the value of a!=b " <<(a!=b)<<endl;
+    cout<<"the value of a%b " <<(a%b)<<endl;
+    cout<<"the value of a>b " <<(a>b)<<endl;
+    cout<<"the value of a<b " <<(a<b)<<endl;
+     cout<<"the value of a++ " <<(a++)<<endl;
+     cout<<"the value of a " <<a<<endl;
+    cout<<"the value of ++a " <<(++a)<<endl;
+
+    // logical operator 
+    cout<<"the value of logical operator (a==b)&&(a<b) =" <<((a==b)&&(a<b))<<endl;
+    cout<<"the value of logical operator (a==b)||(a<b) =" <<((a==b)||(a<b));
+   cout<<"the value of logical operator !(a==b) =" <<(!(a==b))<<endl;
+   
+
+
+        
+
+
+
+
+return 0;
+
+}
